60cfbcbf6cd9802e83555742fd0b75213865f069
Tomasz Rebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2024-03-09 14:56:55 +0100

    add zad3.java Program return pi

---
+++ lab3/zad3.java
@@ -0,0 +1,15 @@
+package lab3;
+
+import java.math.*;
+public class zad3 {
+    public static void main(String[] args)
+    {
+        double wynik = 0;
+        for(int n = 0; n<100000; n++){
+            wynik+=(Math.pow(-1.0,n))/((2*n)+1);
+        }
+        System.out.println(wynik*4);
+        System.exit(0);
+    }
+    }
+
