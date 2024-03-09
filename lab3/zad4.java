2f35c9c211578bd570c2ae2d2b3a49fb973bdb84
TomaszRebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2024-03-09 20:47:29 +0100

    hot fix

--- lab3/zad4.java
+++ lab3/zad4.java
@@ -2,10 +2,10 @@ package lab3;
 import java.math.*;
 public class zad4 {
     public static void main(String[] args) {
-        double wynik = 0.0;
+        double wynik = 1.0;
         for(int n=1;n<10000;n++)
         {
-            wynik *=((2.0*n)/(2*n)-1)*((2*n)/(2*n)+1);
+            wynik *=((2.0*n)/((2*n)-1))*((2.0*n)/((2*n)+1));
         }
         System.out.println(wynik*2);
         System.exit(0);
